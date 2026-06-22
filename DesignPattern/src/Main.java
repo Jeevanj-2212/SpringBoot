142cf0675a09b74dedb0899435fad6cd5404838d
Jeevanj-2212 <jeevanjajekar@gmail.com>
2026-06-22 22:34:41 +0530

    Singleton Pattern

---
+++ DesignPattern/src/Main.java
@@ -0,0 +1,9 @@
+import singlton.TradingPlatformConfig;
+
+public class Main {
+    public static void main(String[] args) {
+      TradingPlatformConfig config = TradingPlatformConfig.getInstance();
+        TradingPlatformConfig config1 = TradingPlatformConfig.getInstance();
+
+    }
+}
